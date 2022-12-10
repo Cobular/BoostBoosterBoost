ddf6429f0631561aeae738f420f5a8d6336bfce6
cobular <22972550+Cobular@users.noreply.github.com>
2022-12-10 10:52:50 -0800

    Removed the dock icon

--- app/src-tauri/src/main.rs
+++ app/src-tauri/src/main.rs
@@ -2,6 +2,10 @@
     all(not(debug_assertions), target_os = "windows"),
     windows_subsystem = "windows"
 )]
+use tauri::{
+    ActivationPolicy, AppHandle, CustomMenuItem, SystemTray, SystemTrayEvent, SystemTrayMenu,
+    SystemTrayMenuItem,
+};
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -9,9 +13,53 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+fn handle_system_tray_event(app: &AppHandle, event: SystemTrayEvent) {
+    match event {
+        SystemTrayEvent::MenuItemClick { tray_id, id, .. } => match id.as_str() {
+            "quit" => {
+                app.exit(0);
+            }
+            "hide" => {
+                app.hide().unwrap();
+            }
+            "show" => {
+                app.show().unwrap();
+            }
+            _ => todo!(),
+        },
+        _ => println!("Unhandled system tray event"),
+    }
+}
+
 fn main() {
-    tauri::Builder::default()
+    let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+    let hide = CustomMenuItem::new("hide".to_string(), "Hide");
+    let show = CustomMenuItem::new("show".to_string(), "Show");
+    let tray_menu = SystemTrayMenu::new()
+        .add_item(quit)
+        .add_native_item(SystemTrayMenuItem::Separator)
+        .add_item(hide)
+        .add_item(show);
+
+    let system_tray = SystemTray::new().with_menu(tray_menu);
+
+    let app = tauri::Builder::default()
+        .setup(|app| {
+            // Hides the dock icon on macOS. Barely documented, see
+            // https://github.com/tauri-apps/tauri/issues/4852#issuecomment-1312716378
+            app.set_activation_policy(ActivationPolicy::Accessory);
+            Ok(())
+        })
+        .system_tray(system_tray)
+        .on_system_tray_event(handle_system_tray_event)
         .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+        .build(tauri::generate_context!())
+        .expect("error while building tauri application");
+
+    app.run(|_app_handle, event| match event {
+        tauri::RunEvent::ExitRequested { api, .. } => {
+            api.prevent_exit();
+        }
+        _ => (),
+    });
 }
